7a10443ffc0616401e19515106aed30a25d96e63
hichamDe <hicham.iamiri200@gmail.com>
2025-05-28 19:48:00 +0100

    added <Analytics/>

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${nunito.className} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
